9ab284bbbc2a23055b6d4e72802b607c4f07be55
dagr27 <00118717@uca.edu.sv>
2020-05-02 23:43:34 -0600

    Corrigiendo errores y subiendo version final

--- src/main/java/com/uca/labo3T/capasLaboTarea3/controller/ProductController.java
+++ src/main/java/com/uca/labo3T/capasLaboTarea3/controller/ProductController.java
@@ -14,31 +14,34 @@ import com.uca.labo3T.capasLaboTarea3.domain.Product;
 public class ProductController {
 	private List<Product> ball = new ArrayList<Product>();
 	
-	@GetMapping("/fillProduct")
-	public ModelAndView fillBall() {
+	@GetMapping("/buyBall")
+	public ModelAndView buyBall() {
 		ModelAndView mav = new ModelAndView();
 		ball.add(new Product(0, "Soccer Ball" , "10"));
 		ball.add(new Product(1, "Bkb Ball" , "15"));
 		ball.add(new Product(2, "PingPong Ball" , "5"));
 		mav.setViewName("home");
-		mav.addObject("producto", ball);
+		mav.addObject("product", new Product());
+		mav.addObject("ballP", ball);
 		return mav;
 	}
 	
 	@PostMapping("/validate")
 	public ModelAndView validate(Product ballP) {
 		ModelAndView mav = new ModelAndView();
-		Integer amount = Integer.parseInt(ballP.getCantidad());
 		mav.addObject("producto", ball.get(ballP.getId()).getNombre());
+		Integer amount = Integer.parseInt(ballP.getCantidad());
 		Integer ballAmount = Integer.parseInt(ball.get(ballP.getId()).getCantidad());
 		if( amount > ballAmount) {
+			mav.addObject("nombre", ball.get(ballP.getId()).getNombre());
 			mav.setViewName("error");
 			
 		}else {
 			Integer total = ballAmount - amount;
-			ballP.setCantidad(total.toString());
-			ball.set(ballP.getId(), ballP);
+			mav.addObject("nombre", ball.get(ballP.getId()).getNombre());
+			mav.addObject("total",total.toString());
 			mav.setViewName("buy");
+			
 		}
 		return mav;	
 	}
